0bb963995bd95f4a64a3b285d20edfba03fd9604
AhmadN123R <ahmadibnunizar41@gmail.com>
2025-08-22 08:24:56 +0700

    memindahkan footer dari index.php ke footer.php

--- index.php
+++ index.php
@@ -64,13 +64,5 @@
             ?>
         </div>
     </div>
-
-    <!-- Footer -->
-    <footer class="footer text-center py-3 bg-dark text-light mt-auto">
-      © 2025 Belajar PHP Native | Dibuat dengan ❤️ dan PHP
-    </footer>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- modul/layouts/footer.php
+++ modul/layouts/footer.php
@@ -1,2 +1,6 @@
-<hr style="border-color: rgba(255,255,255,0.2);">
-<p>&copy; 2025 Belajar PHP Native | Dibuat dengan ❤️ dan PHP</p>
+<footer class="footer text-center py-3 bg-dark text-light mt-auto">
+      © 2025 Belajar PHP Native | Dibuat dengan ❤️ dan PHP
+    </footer>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
